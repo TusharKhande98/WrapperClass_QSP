cf580ae3d775d31ff01da324304fab9a9a633942
Tushar Khande <90444989+TusharKhande98@users.noreply.github.com>
2023-07-26 09:10:51 +0530

    Add files via upload

---
+++ QSP1_Boxing.java
@@ -0,0 +1,12 @@
+public class QSP1_Boxing
+{
+    public static void main(String[] args)
+    {
+        // Below declaration type is depricated...
+        Character c1 = new Character('t'); // boxing
+        System.out.println(c1); // non-primitive
+        char ch1 = 't';
+        System.out.println(ch1); // primitive
+
+    }
+}
