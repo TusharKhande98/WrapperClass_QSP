d643bc0d26f31450c9e7d36255b85e2c048749f0
Tushar Khande <90444989+TusharKhande98@users.noreply.github.com>
2023-08-04 08:50:20 +0530

    Add files via upload

---
+++ QSP3_Unboxing.java
@@ -0,0 +1,19 @@
+public class QSP3_Unboxing
+{
+
+    public static void main(String[] args)
+    {
+        Integer i1 = new Integer(678); // boxing
+        int i = i1.intValue(); // unboxing
+        System.out.println(i);
+
+        Character c1 = new Character('t'); // boxing
+        char ch = c1.charValue(); // unboxing
+        System.out.println(ch); // non-primitive
+
+        Double d = new Double(345.123); // boxing
+        double d1 = d.doubleValue(); // unboxing
+        System.out.println(d1);
+
+    }
+}
