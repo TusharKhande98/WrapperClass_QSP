0425c038113cfdd1b33da8c257aaff46eb55efdf
Tushar Khande <90444989+TusharKhande98@users.noreply.github.com>
2023-08-06 08:50:28 +0530

    Add files via upload

---
+++ QSP4_AutoBoxing.java
@@ -0,0 +1,10 @@
+public class QSP4_AutoBoxing
+{
+    public static void main(String[] args)
+    {
+        Integer i = 10; // Auto Boxing
+        int i1 = i; // Auto Unboxing
+
+        System.out.println(i1);
+    }
+}
